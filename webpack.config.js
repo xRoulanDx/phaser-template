118f6dbeb9aec0554e9c19dac8ef15c4a580a44c
v.konoplev <v.konoplev@tinkoff.ru>
2019-02-06 19:20:37 +0300

    Add prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,23 @@
 var path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: './src/game.ts',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      { test: /\.ts$/, loader: 'ts-loader', exclude: '/node_modules/' }
-    ]
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, './dist/'),
-    host: '127.0.0.1',
-    port: 8080,
-    open: true
-  },
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
-  plugins: [
-    new CopyWebpackPlugin(['index.html'])
-  ]
+	entry: './src/game.ts',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [{test: /\.ts$/, loader: 'ts-loader', exclude: '/node_modules/'}]
+	},
+	devServer: {
+		contentBase: path.resolve(__dirname, './dist/'),
+		host: '127.0.0.1',
+		port: 8080,
+		open: true
+	},
+	resolve: {
+		extensions: ['.ts', '.js']
+	},
+	plugins: [new CopyWebpackPlugin(['index.html'])]
 };
